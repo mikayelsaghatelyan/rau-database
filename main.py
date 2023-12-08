d3fdbc5b1649c101a693c2af6ffa79c1032ad1d8
Mikayel Saghatelyan <mikaelsaghatelyan@gmail.com>
2023-12-08 13:12:59 +0400

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import crud
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
